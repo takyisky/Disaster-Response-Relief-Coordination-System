49c6c1f4ce48da9949a3a020a82987ed729fce22
Emmanuel Obodai <150862905+takyisky@users.noreply.github.com>
2026-01-29 09:35:34 +0000

    Initial creation and startup of agent.

---
+++ dummyagent2.py
@@ -0,0 +1,45 @@
+import asyncio
+import spade
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+
+class DummyAgent(Agent):
+    class MyBehav(CyclicBehaviour):
+        async def on_start(self):
+            print("Starting behaviour . . .")
+            self.counter = 0
+
+        async def run(self):
+            print("Counter: {}".format(self.counter))
+            self.counter += 1
+            if self.counter > 3:
+                self.kill(exit_code=10)
+                return
+            await asyncio.sleep(1)
+
+        async def on_end(self):
+            print("Behaviour finished with exit code {}.".format(self.exit_code))
+
+    async def setup(self):
+        print("Agent starting . . .")
+        self.my_behav = self.MyBehav()
+        self.add_behaviour(self.my_behav)
+
+async def main():
+    dummy = DummyAgent("takyisky.agent1@xmpp.jp", "agent1")
+    await dummy.start()
+
+    # wait until user interrupts with ctrl+C
+    while not dummy.my_behav.is_killed():
+        try:
+            await asyncio.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    assert dummy.my_behav.exit_code == 10
+
+    await dummy.stop()
+
+
+if __name__ == "__main__":
+        spade.run(main())
